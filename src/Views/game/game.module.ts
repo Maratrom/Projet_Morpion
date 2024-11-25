5253f00b78260131391a26e2f128e110bbb72c16
Swixos <erazion.swixo@gmail.com>
2024-11-25 16:00:25 +0100

    fix

---
+++ src/Views/game/game.module.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-game',
+  templateUrl: './game.module.ts',
+  styleUrl: './game.module.scss',
+})
+export class appGame {}

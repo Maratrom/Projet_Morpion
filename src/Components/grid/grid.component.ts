b635bd08a029632700ef98b298550fa040813b9c
Maratrom <93550389+Maratrom@users.noreply.github.com>
2024-11-25 11:59:11 +0100

    Setup base

---
+++ src/Components/grid/grid.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-grid',
+  templateUrl: './grid.component.ts',
+  styleUrl: './grid.component.scss',
+})
+export class Grid {}
